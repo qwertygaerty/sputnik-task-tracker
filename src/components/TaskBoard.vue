8a066dc16e9c54ba3952226fadfc5049dc0d6992
qwertygaerty <54385591+qwertygaerty@users.noreply.github.com>
2022-05-01 18:48:46 +0700

    пофиксил скроллбар

--- src/components/TaskBoard.vue
+++ src/components/TaskBoard.vue
@@ -2,12 +2,10 @@
   <main class="project">
     <div class="project-info">
       <h1>{{ board.name }}</h1>
-
-      <div class="project-column">
-        <a href="#" class="btn" @click="addColumn">Добавить столбец</a>
-      </div>
-
       <div class="project-participants">
+        <a href="#" class="btn" style="margin-right: 1rem" @click="addColumn"
+          >Добавить столбец</a
+        >
         <span></span>
         <span></span>
         <span></span>
@@ -34,6 +32,11 @@
               {{ i.name }}
             </h2>
             <div class="task-boards--edit-panel">
+              <a
+                href="#"
+                class="task-remove project-participants__add"
+                @click="addCard(i)"
+              ></a>
               <a href="#" class="btn task-remove" @click="editBoard(i)">
                 <svg class="svg-icon" viewBox="0 0 20 20">
                   <path
@@ -47,12 +50,6 @@
                   <path
                     d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"
                   ></path></svg
-              ></a>
-
-              <a
-                href="#"
-                class="task-remove project-participants__add"
-                @click="addCard(i)"
               ></a>
             </div>
           </div>
@@ -184,6 +181,9 @@ export default defineComponent({
       massOfEl.forEach((el) => {
         if (el.getAttribute("animation") === "100") {
           el.classList.toggle("min-height-40");
+          let b = Array.from(document.getElementsByTagName("body"));
+          b[0].style.overflow =
+            b[0].style.overflow === "hidden" ? "auto" : "hidden";
         }
       });
       return 0;
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,23 @@ import router from "./router";
 import "@imengyu/vue3-context-menu/lib/vue3-context-menu.css";
 import ContextMenu from "@imengyu/vue3-context-menu";
 import "../src/assets/css/iconfont.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHYojo-ARk04vfDB1fQ5JxvWKdtFA3vUs",
+  authDomain: "sputnik-task-tracker.firebaseapp.com",
+  projectId: "sputnik-task-tracker",
+  storageBucket: "sputnik-task-tracker.appspot.com",
+  messagingSenderId: "767101755102",
+  appId: "1:767101755102:web:41d20db26add997ea6ac4f",
+  measurementId: "G-JD9M2CXHX5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 createApp(App).use(router).use(ContextMenu).mount("#app");
